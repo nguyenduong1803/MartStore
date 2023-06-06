a3386e59a242969484d6447b1181018408742de5
duonnh200 <duongnh@sphinxjsc.com>
2023-06-06 10:09:50 +0700

    feat config atlas db online

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jSDoc";
 import productRoute from "./route/product.route";
 import morgan from "morgan";
 import mongoose from "mongoose";
@@ -14,26 +13,13 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 // nhúng
+const dbUrl = process.env.ATLAS_URL
+const clientUrl = process.env.CLIENT_URL
 const app = express();
 app.use(cors());
-const options = {
-  definition: {
-    openapi: "3.0.3",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-    },
-    servers: [
-      {
-        url: "http://localhost:3002",
-      },
-    ],
-  },
-  apis: ["./route/*.js", "./controllers/*.js", "./models/*.js"],
-};
+
 mongoose
-  .connect("mongodb://0.0.0.0:27017/smartStore")
+  .connect(dbUrl)
   .then(() => console.log("ket nối database thành công"))
   .catch((err) => console.log("kết nối database thất bại", err));
 
@@ -41,12 +27,11 @@ morgan("tiny");
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3002"],
+    origin: [clientUrl],
   })
 );
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Origin", "https://freetuts.net");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -62,8 +47,6 @@ app.use("/api", orderRoute);
 app.use("/api", blogRouter);
 app.use("/api", projectRoute);
 app.use("/api", taskRoute);
-const specs = swaggerJSDoc(options);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.listen(process.env.PORT || 3000, () => {
   console.log("server start PORT: ", process.env.PORT);
 });
